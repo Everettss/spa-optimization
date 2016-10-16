949d916d5b1a9a0a300e07d42a4a17d05cebe64d
Michal Janaszek <mjanasze@intercars.eu>
2016-10-16 18:37:17 +0200

    stage3 server side rendering

--- src/server.js
+++ src/server.js
@@ -1,29 +1,37 @@
 
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { Router, match, createMemoryHistory } from 'react-router';
+import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import routes from './routes';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '..', 'build', 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
-    fs.readFile('./build/webpack-assets.json', 'utf8', (err, manifest) => {
-        const { js, css } = JSON.parse(manifest).main;
+    const history = createMemoryHistory(req.url);
+    match({ history, routes }, (error, redirectLocation, renderProps) => {
+        let body = ReactDOMServer.renderToString(<Router {...renderProps} />);
+        fs.readFile('./build/webpack-assets.json', 'utf8', (err, manifest) => {
+            const { js, css } = JSON.parse(manifest).main;
 
-        const html =
-`<html>
-    <head>
-        <link rel="icon" href="data:;base64,iVBORw0KGgo=">
-        <link rel="stylesheet" type="text/css" href="${css}">
-        <script async src="${js}"></script>
-    </head>
-    <body>
-        <div id="root">Loading</div>
-    </body>
-</html>`;
-        res.status(200).send(html);
+            const html =
+                `<html>
+                    <head>
+                        <link rel="icon" href="data:;base64,iVBORw0KGgo=">
+                        <link rel="stylesheet" type="text/css" href="${css}">
+                        <script async src="${js}"></script>
+                    </head>
+                    <body>
+                        <div id="root">${body}</div>
+                    </body>
+                </html>`;
+            res.status(200).send(html);
+        });
     });
 });
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,41 @@ const path = require('path');
 const AssetsPlugin = require('assets-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 const DEBUG = !process.argv.includes('--release');
 
-module.exports = {
+const loaders = [
+    {
+        exclude: /node_modules/,
+        loader: 'babel',
+    },
+    {
+        test: /\.js$/,
+        loader: `eslint-loader${DEBUG ? '?{rules:{"no-console":0}}' : ''}`,
+        exclude: /node_modules/,
+    },
+];
+
+const urlLoader = {
+    test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+    loader: 'url-loader',
+    query: {
+        emitFile: true,
+        limit: 10000,
+        name: DEBUG ? '[name].[ext]' : '[hash].[ext]',
+    }
+};
+
+const styleLader =  {
+    test: [/\.scss$/, /\.css$/],
+    loader: ExtractTextPlugin.extract(
+        'style-loader',
+        'css-loader!sass-loader'),
+};
+
+
+const client = {
     entry: path.resolve(__dirname, 'src', 'client.js'),
     output: {
         path: path.resolve(__dirname, 'build', 'public'),
@@ -15,41 +46,24 @@ module.exports = {
         filename: DEBUG ? 'bundle.js' : '[hash].js',
         chunkFilename: DEBUG ? '[id].chunk.js' : '[chunkhash].js',
     },
+    target: 'web',
+    node: {
+        fs: 'empty',
+    },
     module: {
         loaders: [
-            {
-                exclude: /node_modules/,
-                loader: 'babel',
-            },
-            {
-                test: [/\.scss$/, /\.css$/],
-                loader: ExtractTextPlugin.extract(
-                    'style-loader',
-                    'css-loader!sass-loader'),
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
-                loader: 'url-loader',
-                query: {
-                    emitFile: true,
-                    limit: 10000,
-                    name: DEBUG ? '[name].[ext]' : '[hash].[ext]',
-                }
-            },
-            {
-                test: /\.js$/,
-                loader: `eslint-loader${DEBUG ? '?{rules:{"no-console":0}}' : ''}`,
-                exclude: /node_modules/,
-            },
+            ...loaders,
+            styleLader,
+            urlLoader
         ]
     },
     devtool: DEBUG ? 'eval-source-map' : false,
     plugins: [
         new AssetsPlugin({ path: path.join(__dirname, 'build')}),
-        new CleanWebpackPlugin(['build'], {
+        new CleanWebpackPlugin(['build/public'], {
             root: process.cwd(),
         }),
-        new ExtractTextPlugin(DEBUG ? 'style.css' : '[contenthash].css'),
+        new ExtractTextPlugin(DEBUG ? 'style.css' : '[contenthash].css', { allChunks: true }),
         new webpack.optimize.CommonsChunkPlugin({
             name: 'main',
             minChunks: 2,
@@ -71,3 +85,48 @@ module.exports = {
         ] : []),
     ],
 };
+
+const server = {
+    entry: './src/server.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'server.js',
+        publicPath: '/',
+    },
+    target: 'node',
+    module: {
+        loaders: [
+            ...loaders,
+            {
+                test: [/\.scss$/, /\.css$/],
+                loader: 'ignore-loader',
+            },
+            Object.assign({}, urlLoader, {
+                query: Object.assign({}, urlLoader.query, {
+                    emitFile: false,
+                })
+            })
+        ],
+    },
+    plugins: [
+        new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1,
+        })
+    ],
+    node: {
+        console: false,
+        global: false,
+        process: false,
+        Buffer: false,
+        __filename: false,
+        __dirname: false,
+    },
+    externals: [nodeExternals({
+        whitelist: [/\.css/],
+    })],
+};
+
+module.exports = [
+    client,
+    server,
+];
