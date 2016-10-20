9bf2beefc2c3f6c34715987aee17d4d81e390de6
Michal Janaszek <mjanasze@intercars.eu>
2016-10-20 15:59:06 +0200

    stage3 simplify names of produced js and css files

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ import ReactDOMServer from 'react-dom/server';
 import { Router, match, createMemoryHistory } from 'react-router';
 import express from 'express';
 import path from 'path';
-import fs from 'fs';
 import routes from './routes';
 
 const app = express();
@@ -16,22 +15,18 @@ app.get('*', (req, res) => {
     const history = createMemoryHistory(req.url);
     match({ history, routes }, (error, redirectLocation, renderProps) => {
         let body = ReactDOMServer.renderToString(<Router {...renderProps} />);
-        fs.readFile('./build/webpack-assets.json', 'utf8', (err, manifest) => {
-            const { js, css } = JSON.parse(manifest).main;
-
-            const html =
-                `<html>
-                    <head>
-                        <link rel="icon" href="data:;base64,iVBORw0KGgo=">
-                        <link rel="stylesheet" type="text/css" href="${css}">
-                        <script async src="${js}"></script>
-                    </head>
-                    <body>
-                        <div id="root">${body}</div>
-                    </body>
-                </html>`;
-            res.status(200).send(html);
-        });
+        const html =
+            `<html>
+                <head>
+                    <link rel="icon" href="data:;base64,iVBORw0KGgo=">
+                    <link rel="stylesheet" type="text/css" href="style.css">
+                    <script async src="bundle.js"></script>
+                </head>
+                <body>
+                    <div id="root">${body}</div>
+                </body>
+            </html>`;
+        res.status(200).send(html);
     });
 });
 
