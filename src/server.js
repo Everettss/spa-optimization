9951d42445cece034f4cca6a0bc9a40ebefd6268
Michal Janaszek <mjanasze@intercars.eu>
2016-11-07 19:49:14 +0100

    stage2 move stage indicator after #root

--- src/server.js
+++ src/server.js
@@ -15,8 +15,8 @@ app.get('*', (req, res) => {
                 <link rel="stylesheet" href="/style.css">
             </head>
             <body>
-                <div class="stage">2</div>
                 <div id="root"><div class="loading-msg">Loading</div></div>
+                <div class="stage">2</div>
                 <script async src="/bundle.js"></script>
             </body>
         </html>`;
